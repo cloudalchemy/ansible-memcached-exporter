0959f3402535b71a0bcbb0e3130cd1feab855cc0
Matthias Antierens <matthias.antierens@inuits.eu>
2020-11-17 16:53:35 +0100

    fixing python test linter

--- molecule/alternative/tests/test_alternative.py
+++ molecule/alternative/tests/test_alternative.py
@@ -1,9 +1,9 @@
-import pytest
 import os
 import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
 
 def test_service(host):
     s = host.service("memcached_exporter")
