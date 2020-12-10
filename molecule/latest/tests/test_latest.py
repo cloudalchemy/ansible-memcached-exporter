1a933c4fcedcf3875682cf1c4d892c9635c11e4c
paulfantom <pawel@krupa.net.pl>
2020-12-10 12:49:25 +0100

    molecule: fix tests

--- molecule/latest/tests/test_latest.py
+++ molecule/latest/tests/test_latest.py
@@ -4,3 +4,13 @@ import testinfra.utils.ansible_runner
 
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
+
+
+@pytest.mark.parametrize("files", [
+    "/etc/systemd/system/memcached_exporter.service",
+    "/usr/local/bin/memcached_exporter"
+])
+def test_files(host, files):
+    f = host.file(files)
+    assert f.exists
+    assert f.is_file
